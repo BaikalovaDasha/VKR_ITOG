c1eab748a20fb7256d028daf6d0df052975159eb
BaikalovaDasha <baikalova0207@mail.ru>
2024-01-07 13:39:01 +0700

    feature(View) : добавлен расчётный модуль, в котором выполяется расчёт средних коэффициентов выработки мощности для каждого определённого режима

---
+++ View/CalculationModel/GetAveargeCoef.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationModel
+{
+    internal class GetAveargeCoef
+    {
+    }
+}
